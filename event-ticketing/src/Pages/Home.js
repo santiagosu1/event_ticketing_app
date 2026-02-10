ae05fce2fc5d879b781a6aa09d63ca25de08fcbe
Javier Nieto <javiale0310@gmail.com>
2026-02-09 19:46:49 -0800

    update

--- event-ticketing/src/Pages/Home.js
+++ event-ticketing/src/Pages/Home.js
@@ -1,10 +1,41 @@
-    import { useContext } from "react";
-    import { AppContext } from "../AppContext";
+import React from "react";
+import HeroCarousel from "../components/HeroCarousel/HeroCarousel";
+import EventsSection from "../components/EventsSection/EventsSection";
+import Categories from "../components/Categories/Categories";
+import eventsData from "../data/events.json";
+import "./Home.css";
 
+export default function Home() {
+    const { heroSlides, cantMissPlans, recommendedEvents } = eventsData;
 
-    export default function Home() {
-        const {user} = useContext(AppContext);
-        return (
-            <h1>Home Page {user}</h1>
-        );
-    }
+    return (
+        <div className="home-page">
+            <div className="home-container">
+                {/* Hero Carousel */}
+                <HeroCarousel slides={heroSlides} />
+
+                {/* Can't Miss Plans Section */}
+                <EventsSection
+                    subtitle="PLANS"
+                    title="CAN'T MISS"
+                    events={cantMissPlans}
+                    carousel={true}
+                    sortByDate={true}
+                    overlayNav={false}
+                />
+
+                {/* Recommended Events Section */}
+                <EventsSection
+                    subtitle="EVENTS YOU MIGHT"
+                    title="LIKE"
+                    events={recommendedEvents}
+                    note="We suggest events based on your previous purchases and searches."
+                    carousel={false}
+                />
+
+                {/* Categories Section */}
+                <Categories />
+            </div>
+        </div>
+    );
+}
